a9bd3134104f7ddda25c63e4781732613ccef912
cattanidudu <44449786+cattanidudu@users.noreply.github.com>
2018-10-24 18:48:18 -0300

    24/OUTUBRO

--- autocone_prototypes/colin/autocone_colin_decision/scripts/line_follower.py
+++ autocone_prototypes/colin/autocone_colin_decision/scripts/line_follower.py
@@ -24,179 +24,221 @@ import time
 import collections
 import time
 
+
 class vision():
 
-    def __init__(self):
-        self.kernel = np.ones((3,3),np.uint8)
-        self.cap = cv2.VideoCapture(1)
-        self.buffer_pontos = []
-        self.buffer_size = 5
-        self.lista_erros = []
-        #self.ransac = linear_model.RANSACRegressor()
+	def __init__(self):
+		self.kernel = np.ones((3,3),np.uint8)
+		#self.cap = cv2.VideoCapture("/home/dudu/usb_camimage_raw.mp4")
+		self.cap = cv2.VideoCapture(1)
 
+		#self.cap = cv2.VideoCapture("/home/dudu/Dropbox/Programming/line_follower/3.mp4")
+		#for x in range(200):
+		#	_ = self.cap.read()
+		self.buffer_pontos = []
+		self.buffer_size = 5
+		self.lista_erros = []
+		self.lista_erros_ransac = []
+		#self.Hmatrix = np.array([[-3.90976610e+00,-5.90540798e+00,1.65325824e+03],[-1.24055090e+00,-2.04359973e+01,3.41047888e+03],[-1.22651441e-03,-1.17716292e-02,1.00000000e+00]])
+		self.Hmatrix = np.array([[2.88501164e+01,4.90100565e+01,-9.66583626e+03],[-9.59281605e-01,1.49645379e+02,-2.18268875e+04],[2.27372670e-03,1.54124011e-01,1.00000000e+00]])
 
+		self.sf = 5
 
+		h_image = 120
+		w_image = 120
+		h_image*= self.sf
+		w_image*= self.sf
 
-    def find_error(self):
+		self.lista_erros = []
+		self.h_image = h_image
+		self.w_image = w_image
+
+		self.last_valid_error = 0
+		self.find_Hmatrix()
+		#self.cap = cv2.VideoCapture("/home/dudu/Dropbox/Programming/line_follower/3.mp4")
 		
-	erro=0
-        ret, image = self.cap.read()
-#	print image
-        time_1 = time.time()
-        image = cv2.resize(image,(0,0),fx=0.5,fy=0.5)
-	print_image = image.copy()
-        image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-        mean_image = image.copy()
-        original = image.copy()
+		#self.ransac = linear_model.RANSACRegressor()
+
+
+	def find_Hmatrix(self):
+		while True:
+			_, image = self.cap.read()
+			cv2.imshow("image",image)
+			if cv2.waitKey(0) == 27: break
+
+		lista_entrada = []
+		for x in range(4):
+			lista_entrada.append(cv2.selectROI('MultiTracker',image)[:4])
+		
+		lista_entrada_original = lista_entrada
+		lista_entrada = np.array(lista_entrada_original).reshape(1,4,4).astype(np.float32)
+		lista_entrada = lista_entrada[0,:,:2]+lista_entrada[0,:,2:]/2
+		lista_entrada = np.array(lista_entrada).reshape(1,4,2).astype(np.float32)
+
+
+
+		h_linha = 30.*self.sf
+		w_linha = 5.*self.sf
+
+		h_image = self.h_image 
+		w_image = self.w_image 
+
+		lista_referencia_linha = np.array([[(w_image-w_linha)/2,h_image-2*h_linha,0]
+										   ,[(w_image-w_linha)/2+w_linha,h_image-2*h_linha,0]
+										   ,[(w_image-w_linha)/2+w_linha,h_image-1*h_linha,0]
+										   ,[(w_image-w_linha)/2,h_image-1*h_linha,0]
+										  ]).reshape(1,4,3).astype(np.float32)
+
+		h, status = cv2.findHomography(lista_entrada, lista_referencia_linha)
+		print np.array(h)
+		#self.cap = cv2.VideoCapture("/home/dudu/Dropbox/Programming/line_follower/teste.mp4")
+		self.Hmatrix = h
+
+
+	def find_error(self):
+		#print "oi"
+		erro=0
+		ret, image = self.cap.read()
+		imagem_copia_original = image.copy()
+		time_1 = time.time()
+		image = cv2.warpPerspective(image, self.Hmatrix, (self.w_image, self.h_image))
+		
+
+		image = cv2.resize(image,(0,0),fx=0.5,fy=0.5)
+		
+		print_image = image.copy()
+		image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+		curve = np.zeros(image.shape,np.uint8)
+		mean_image = image.copy()
+		original = image.copy()
 	
-        image = np.float32(image)
-        image = cv2.GaussianBlur(image,(5,5),0)
-        image = cv2.adaptiveThreshold(image.astype(np.uint8),255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,5,3)
-        image = cv2.erode(image,self.kernel,iterations = 1)
-        image = cv2.dilate(image,self.kernel,iterations = 1)
-        total_area = image.shape[0]*image.shape[1]
-        _,contours,_ = cv2.findContours(image, 1, 2)
+		image = np.float32(image)
+		#image = cv2.medianBlur(image,5)
+		#image = cv2.GaussianBlur(image,(5,5),0)
+		image = cv2.adaptiveThreshold(image.astype(np.uint8),255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,9,3)
+		image = cv2.erode(image,self.kernel,iterations = 2)
+		image = cv2.dilate(image,self.kernel,iterations = 1)
+		total_area = image.shape[0]*image.shape[1]
+		_,contours,_ = cv2.findContours(image, 1, 2)
 
-        lista_contornos = []
-        for cnt in contours:
-            area = cv2.contourArea(cnt)
-            rect = cv2.minAreaRect(cnt)
-            box = cv2.boxPoints(rect)
-            box = np.int0(box)
+		lista_contornos = []
+		for cnt in contours:
+			area = cv2.contourArea(cnt)
+			rect = cv2.minAreaRect(cnt)
+			box = cv2.boxPoints(rect)
+			box = np.int0(box)
+			#print box
+			a = box[0]-box[1]
+			b = box[1]-box[2]
+			vector = (np.linalg.norm(a),np.linalg.norm(b))
+			largura = min(vector)#/np.linalg.norm(b)
+			altura = max(vector)
+			#print a, np.linalg.norm(a)
+			if largura<8*self.sf/2 and largura>3*self.sf/2 and altura>20*self.sf/2:
+				lista_contornos.append(box)	
 
-            a = box[0]-box[1]
-            b = box[1]-box[2]
-            ratio = np.linalg.norm(a)/np.linalg.norm(b)
-            if ratio<1: ratio = ratio**(-1)
-            if area>total_area/500 and area<total_area/20 and ratio>1.5 and ratio <10:
-                lista_contornos.append(cnt)
+			"""
+			if ratio<1: ratio = ratio**(-1)
+			if area>total_area/500 and area<total_area/20:# and ratio>1.5 and ratio <10:
+				lista_contornos.append(cnt)
+			"""
 		
 		
-        contours = lista_contornos
-        lista_contornos = []
-        pontos_frame = []
-        #cv2.drawContours(original, contours, -1, (0,0,255), 3)
+		contours = lista_contornos
+		lista_contornos = []
+		pontos_frame = []
+		cv2.drawContours(original, contours, -1, (0,0,255), 3)
 	
-        #vencedor = (image.shape[1],None) #ycentro, x0
-        for cnt in contours:
-            epsilon = 0.01*cv2.arcLength(cnt,True)
-            approx = cv2.approxPolyDP(cnt,epsilon,True)
-            mask = np.zeros(image.shape,np.uint8)
-            cv2.drawContours(mask,[cnt],0,255,-1)
-            mean_val = cv2.mean(mean_image,mask = mask)
-	    
-            """
-            try:
-                ,,angle = cv2.fitEllipse(cnt)
-                if angle>90: angle=180-angle
-            except:
-                angle = 90
-            """
+		#vencedor = (image.shape[1],None) #ycentro, x0
+		for cnt in contours:
+			#epsilon = 0.01*cv2.arcLength(cnt,True)
+			#approx = cv2.approxPolyDP(cnt,epsilon,True)
+			mask = np.zeros(image.shape,np.uint8)
+			cv2.drawContours(mask,[cnt],0,255,-1)
+			mean_val = cv2.mean(mean_image,mask = mask)
+		
+			"""
+			try:
+				,,angle = cv2.fitEllipse(cnt)
+				if angle>90: angle=180-angle
+			except:
+				angle = 90
+			"""
 			
 			#   len(approx)>3 and len(approx)<10 and 
 
-            #print(mean_val[0])		
+			#print(mean_val[0])		
 
-            if mean_val[0]>150 and len(approx)>3: #and len(approx)<8:
-		cv2.drawContours(print_image,[	approx],-1,(255,255,0),5)
-                M = cv2.moments(approx)
-		cx = int(M['m10']/M['m00'])
-		cy = int(M['m01']/M['m00'])  
-		pontos_frame.append([cx,cy])
-		lista_contornos.append([approx])
-
-                #if cy<vencedor[0]:
-                    #cruzay = int(vx/abs(vy)*cy)
-                    #vencedor = (cy,approx)
-                #print righty
-		
-
-	if len(pontos_frame)>0:
-		self.buffer_pontos.append(pontos_frame)
-	elif len(self.buffer_pontos)>0:
-		self.buffer_pontos.pop(0)
-	
-	if len(self.buffer_pontos)>self.buffer_size:
-		self.buffer_pontos.pop(0)
-
-	lista_pontos_buffer = []
-
-	for frame in self.buffer_pontos:
-		for ponto in frame:
-			lista_pontos_buffer.append(ponto)
-	
-	pontos_frame = np.array(pontos_frame).reshape(-1,2)
-
-	lista_pontos_buffer = np.array(lista_pontos_buffer).reshape(-1,2)
-	if lista_pontos_buffer.shape[0]>0:
-		db = DBSCAN(eps=20, min_samples=4).fit(lista_pontos_buffer)
-		labels = db.labels_
-		#print labels
-		n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-		if len(pontos_frame)==0:
-			labels_frame = []
-		else:
-			labels_frame = labels[-len(pontos_frame):]
-		
-		center_contours = []
-		center_clusters = []
-		for cluster in range(n_clusters_):
-			#print pontos_frame.shape, labels_frame.shape, cluster
-			pontos_centro = pontos_frame[labels_frame==cluster]
-			if pontos_centro.shape[0]>0:
-				center_clusters.append(pontos_centro[-1])#np.mean(pontos_frame,axis=0))
-				#center_clusters = np.array(center_clusters).reshape(-1,2)
+			if mean_val[0]>200: #and len(approx)>3: #and len(approx)<8:
+				cv2.drawContours(print_image,[cnt],-1,(255,255,0),5)
+				M = cv2.moments(cnt)
+				cx = int(M['m10']/M['m00'])
+				cy = int(M['m01']/M['m00'])  
+				pontos_frame.append([cx,cy])
+				lista_contornos.append([cnt])
+				rows,cols = image.shape[:2]
+				[vx,vy,x,y] = cv2.fitLine(cnt, cv2.DIST_L2,0,0.01,0.01)
+				lefty = int((-x*vy/vx) + y)
+				righty = int(((cols-x)*vy/vx)+y)
+				#try:
+				mask_curve = np.zeros(image.shape,np.uint8)
+				try:
+					#cv2.line(mask_curve,(cols-1,righty),(0,lefty),(255),2)
+					#curve[mask==255] = mask_curve[mask==255]
+					curve +=mask
+				except:
+					pass
+		try:
+			points = np.nonzero(curve)
+			polinomio = np.poly1d(np.polyfit(points[0],points[1], 2))
 			
-		
-		for ponto in center_clusters:
-			cv2.circle(original,(int(ponto[0]),int(ponto[1])),7,(0,0,255),-1)
-		
-		mais_longe = [0,image.shape[0]]
-		for ponto in center_clusters:
-		    if ponto[1]<mais_longe[1]:
-		        mais_longe=ponto
-		
-		if mais_longe[1] < image.shape[0]:
-		   
-		    try:
-		        erro = 90-(np.arctan(float(mais_longe[1])/float(mais_longe[0]-image.shape[1]/2.)))*180./np.pi
-		    except:
-		        erro = 0
-		"""    
-		    self.lista_erros.append(erro)
-		if len(self.lista_erros)>5:
-		    self.lista_erros.pop(0)
-		
-		if len(self.lista_erros)==5:
-		    
-		    	try:
-			    ransac = linear_model.RANSACRegressor()
-			    ransac.fit(np.arange(5).reshape(-1,1),np.array(self.lista_erros).reshape(-1,1))
-		    	    erro = ransac.predict(5)
-		    	    print "foi"
-		    	except:
-		    	    erro = 0
-	    	
-	    	#self.lista_erros(0)#	    return True
-			#except:
-			#	pass
-		"""
 
+			erro = 90-(np.arctan(float(image.shape[0])/float(polinomio(0)-image.shape[1]/2)))*180/np.pi
+			if erro>90: erro = erro-180
+			
+			#self.lista_erros.append(erro)
+			#if self.lis
+			print erro
+			print len(self.lista_erros)
 
- 
+			#if len(self.lista_erros)>300:
+			#	plt.plot(self.lista_erros,"b")
 				
-        #   erro = 90-(np.arctan(float(image.shape[0])/float(value_x-image.shape[1]/2.)))*180./np.pi
-        if erro ==0: x_value=image.shape[1]/2
-	else: x_value = int(image.shape[1]/2+np.tan(erro/180.*np.pi)*image.shape[0])
-        cv2.line(original,(int(image.shape[1]/2),image.shape[0]),(x_value,0),(255,0,0),3)
-        cv2.imshow('Colin', image)
-        cv2.imshow('image', print_image)
-	cv2.imshow("original",original)
-        cv2.waitKey(1)
-        if erro>90: erro = erro-180
-        #print 1000/((time.time()-time_1)*1000)
-        return erro
+			xp = np.linspace(0, image.shape[0], 300)    
+			for x in xp:    
+			   cv2.circle(curve, (int(polinomio(x)),int(x)),2,(255,0,0),-1)
+		except:
+			pass
+		
+		cv2.imshow('Colin', curve)
+		cv2.imshow('image', print_image)
+		
+		image = cv2.resize(image,(0,0),fx=2,fy=2)
+		cv2.imshow("original",image)
+		#image = cv2.warpPerspective(original, self.Hmatrix.T, (image.shape[1], image.shape[0]))
+		#cv2.imshow("perspectvive", image)
 
+		
+		
+		if erro>90: erro = erro-180
+		if erro!=0: self.last_valid_error = erro
+		if erro == 0: erro = self.last_valid_error
+		try:
+			deslocamento_x = int(np.sin(abs(erro)/180*np.pi)*300)#-imagem_copia_original.shape[1]/2
+			deslocamento_y = int(np.cos(abs(erro)/180*np.pi)*300)
+			if erro<0: deslocamento_x*=-1
+			#print deslocamento,"--------"
+			#if erro<0:
+			xp = imagem_copia_original.shape
+			cv2.line(imagem_copia_original, (xp[1]/2,xp[0]),(deslocamento_x+xp[1]/2,xp[0]-deslocamento_y),(0,0,255),4)
+			cv2.imshow("final", cv2.resize(imagem_copia_original,(0,0),fx=0.5,fy=0.5))
+		except:
+			pass
+		#if cv2.waitKey(1)==27:
+		cv2.waitKey(0)
+		print 1000/((time.time()-time_1)*1000)
+		return erro
+		
 def joy_callback(data):
     global new_data 
 
